e5732b3f6afeaa710643abae1cc57f9cef66456c
Shukhratjon <shukhratjon717@gmail.com>
2024-03-24 13:46:40 +0900

    feat: Initial setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("done!");
+
+import moment from "moment"
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime)
+
+const person:string = "Steve"
+const count: number = 100;
