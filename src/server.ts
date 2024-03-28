b95b2c692ed7d20cf064de147933a80ef1b8dbb0
Shukhratjon <shukhratjon717@gmail.com>
2024-03-28 18:26:36 +0900

    feat: integration of enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,3 @@
-console.log("done!");
+import dotenv from "dotenv";
+dotenv.config();
 
-import moment from "moment"
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime)
-
-const person:string = "Steve"
-const count: number = 100;
