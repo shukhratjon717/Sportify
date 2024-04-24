bc233e3b0a2d5b2ce6620edcef27ca630f5ab839
Shukhratjon <shukhratjon0717@gmail.com>
2024-04-24 13:48:52 +0900

    feat: create productController and routing apis

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import shopController from "./controllers/shop.controller";
+import productController from "./controllers/product.controller";
 
 routerAdmin.get("/", shopController.goHome);
 routerAdmin
@@ -10,12 +11,14 @@ routerAdmin
 routerAdmin
   .get("/signup", shopController.getSignup)
   .post("/signup", shopController.processSignup);
-
-  routerAdmin.get("/logout", shopController.logout)
-
-routerAdmin.get("/check-me", shopController.checkAuthSession)
+routerAdmin.get("/logout", shopController.logout);
+routerAdmin.get("/check-me", shopController.checkAuthSession);
 
 /** Product  */
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct)
+routerAdmin.post("/product/:id", productController.updateChosenProduct)
+
 /** User */
 
 export default routerAdmin;
