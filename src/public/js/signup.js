4d95fc0a358b5c433f7be06023d8329f45f3ed59
Shukhratjon <shukhratjon0717@gmail.com>
2024-05-04 14:21:20 +0900

    feat: signup process validation part2

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,28 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const valiImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!valiImageType.includes(fileType)) {
+        alert("Pease insert only jpeg, jpg, png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
 
 function validatesignupForm() {
   const userNick = $(".user-nick").val();
@@ -21,5 +43,13 @@ if (
 
 if (userPasswod !== confirmPassword) {
   alert("Password is different please check!");
+  return false;
+}
+
+const userImage = $(".user-image").get(0).files[0]
+  ? $(".user-image").get(0).files[0].name
+  : mull;
+if (!userImage) {
+  alert("Please insert shop image!");
   return false;
 }
