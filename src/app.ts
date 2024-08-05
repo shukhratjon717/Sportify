49c2ce8859dd4dca7adb63de93d23759108aee68
Shukhratjon <shukhratjon0717@gmail.com>
2024-08-05 22:03:45 +0900

    feat: develop Socket.io server

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
 
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
@@ -58,4 +60,23 @@ app.set("view engine", "ejs");
 app.use("/admin", routerAdmin); // BSSR; EJS
 app.use("/", router); // SPA: REACT
 
-export default app;
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
+
+let summaryClient = 0;
+io.on("connection", (socket) => {
+  summaryClient++;
+  console.log(`Connection & total [${summaryClient}]`);
+
+  socket.on("disconnect", () => {
+    summaryClient--;
+    console.log(`Disconnection & total [${summaryClient}]`);
+  });
+});
+
+export default server;
--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 import app from "./app";
+import server from "./app";
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 4004;
-    app.listen(PORT, function () {
+    server.listen(PORT, function () {
       console.info(`The server is running successfully on port: ${PORT}`);
       console.info(`Admin project on http://localhost:${PORT}/admin \n`);
     });
